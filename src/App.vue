cb25a1ce11f907fbba5542f1a26e5c0e690a425d
Mutasem <mutdmour@gmail.com>
2021-07-13 08:24:10 +0200

    refactor a bunch

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,8 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component({})
 export default class App extends Vue {}
 </script>
 
