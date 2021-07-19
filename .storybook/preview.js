6b8e3d03f6b57f457b182ec670f719f8c4177d11
Mutasem <mutdmour@gmail.com>
2021-07-19 20:51:51 +0200

    add backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,5 +8,25 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-  }
+  },
+  backgrounds: {
+	values: [
+	  {
+		name: '--color-background-dark',
+		value: 'var(--color-background-dark)',
+	  },
+	  {
+		name: '--color-background-base',
+		value: 'var(--color-background-base)'
+	  },
+	  {
+		name: '--color-background-light',
+		value: 'var(--color-background-light)'
+	  },
+	  {
+		name: '--color-background-lighter',
+		value: 'var(--color-background-xlight)'
+	  }
+	],
+      },
 }
