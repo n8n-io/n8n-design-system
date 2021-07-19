38ce5e009984a4efb9364ae98a3bf8d1be53a75e
Mutasem <mutdmour@gmail.com>
2021-07-19 18:18:26 +0200

    add variables

---
+++ src/styleguide/ColorCircles.vue
@@ -0,0 +1,79 @@
+<template>
+<div :class="$style.section">
+	<div  v-for="color in colors" :key="color" :class="$style.container">
+		<div :class="$style.circle" :style="{backgroundColor: `var(${color})`}"></div>
+		<span>{{color}}</span>
+		<span>{{getHSLValue(color)}}</span>
+		<span>{{getHexValue(color)}}</span>
+	</div>
+</div>
+</template>
+
+<script lang="ts">
+function hslToHex(h: number, s: number, l: number) {
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+function getHex(hsl: string) {
+	const colors = hsl.replace('hsl(', '').replace(')', '').replace(/%/g, '').split(',')
+		.map((n: string) => parseInt(n));
+
+	return hslToHex(colors[0], colors[1], colors[2]);
+}
+
+export default {
+  name: "color-circles",
+  props: {
+    colors: {
+      type: Array,
+      required: true,
+    }
+  },
+  methods: {
+	  getHSLValue(color: string) {
+		const style = getComputedStyle(document.body)
+
+		return style.getPropertyValue(color);
+	  },
+	  getHexValue(color: string) {
+		const hsl = this.getHSLValue(color);
+
+		return getHex(hsl);
+	  },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.section {
+	display: flex;
+}
+
+.name {
+	align-self: center;
+}
+
+.container {
+	width: 140px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+	align-self: flex-start;
+}
+
+.circle {
+	border-radius: 50%;
+	height: 80px;
+	width: 80px;
+	margin-bottom: 5px;
+	border: 1px solid lightgray;
+}
+</style>
---
+++ src/styleguide/_colors.stories.js
@@ -0,0 +1,31 @@
+// import ColorCircles from './ColorCircles.vue';
+
+// export default {
+// 	title: 'Styleguide/_C',
+// 	component: ColorCircles,
+// };
+
+// const Template = (args, {
+// 	argTypes
+// }) => ({
+// 	props: Object.keys(argTypes),
+// 	components: {
+// 		ColorCircles
+// 	},
+// 	template: '<div> <color-circles v-bind="$props" /> </div>',
+// });
+
+// export const Colors = Template.bind({});
+// Colors.args = {
+// 	colors: {
+// 		primary: ['--color-primary-shade-1', '--color-primary', '--color-primary-tint-1', '--color-primary-tint-2', '--color-primary-tint-3'],
+// 		secondary: ['--color-secondary'],
+// 		success: ['--color-success', '--color-success-tint-1', '--color-success-tint-2'],
+// 		warning: ['--color-warning', '--color-warning-tint-1', '--color-warning-tint-2'],
+// 		danger: ['--color-danger', '--color-danger-tint-1', '--color-danger-tint-2'],
+// 		info: ['--color-info', '--color-info-tint-1', '--color-info-tint-2'],
+// 		text: ['--color-text-dark', '--color-text-base', '--color-text-light', '--color-text-lighter', '--color-text-xlight'],
+// 		border: ['--color-foreground-base', '--color-foreground-light'],
+// 		background: ['--color-background-dark', '--color-background-base', '--color-background-light', '--color-background-lighter', '--color-background-xlight']
+// 	}
+// };
