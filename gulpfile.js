4798bb9afc3bd29bc6808c0f2491ad72a6d22017
Mutasem <mutdmour@gmail.com>
2021-07-23 12:48:42 +0200

    remove style dictionary

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,17 @@ const gulp = require("gulp");
 const sass = require("gulp-dart-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
-const StyleDictionary = require("./style-dictionary");
 
 gulp.task(
   "build:theme",
-  gulp.series([compileThemeTokens, compileTheme, copyThemeFonts])
+  gulp.series([compileTheme, copyThemeFonts])
 );
-
-gulp.task("build:theme:tokens", compileThemeTokens);
 
 gulp.task(
   "watch:theme",
   gulp.series([
     "build:theme",
     () => {
-      gulp.watch(
-        "./theme/src/tokens/**/*.json",
-        gulp.series(["build:theme:tokens"])
-      );
       gulp.watch("./theme/src/**/*.scss", gulp.series(["build:theme"]));
     },
   ])
@@ -41,29 +34,6 @@ function compileTheme() {
     .pipe(gulp.dest("./theme/dist"));
 }
 
-function compileThemeTokens(done) {
-  StyleDictionary.build({
-    source: ["theme/src/tokens/base/*.json", "theme/src/tokens/globals/*.json", "theme/src/tokens/components/*.json"],
-    themeSource: [
-      "theme/src/tokens/base/*.json",
-      `theme/src/tokens/themes/:theme/*.json`,
-    ],
-    themes: ["dark"],
-    buildPath: "theme/src/",
-  });
-  done();
-}
-
 function copyThemeFonts() {
   return gulp.src("./theme/src/fonts/**").pipe(gulp.dest("./theme/dist/fonts"));
 }
-
-// function compileTokens(done) {
-// 	StyleDictionary.build({
-// 		source: ['./src/styles/tokens/**/*.json'],
-// 		buildPath: 'src/styles/'
-// 	});
-// 	done();
-// };
-
-// gulp.task('build:tokens', compileTokens);
