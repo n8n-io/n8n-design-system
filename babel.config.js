93085b2c8e5b51918679ce590797b9b4b360e3a2
Mutasem <mutdmour@gmail.com>
2021-07-27 10:31:46 +0200

    remove babel components plugin

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [
-    [
-      "component",
-      {
-        libraryName: "element-ui",
-        styleLibraryName: "~theme",
-      },
-    ],
-  ],
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
