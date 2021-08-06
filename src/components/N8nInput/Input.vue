d30753b743ac4047c53b44c5f9dcae1f5ca2e8ad
Mutasem <mutdmour@gmail.com>
2021-08-06 16:15:44 +0200

    initial implementation

---
+++ src/components/N8nInput/Input.vue
@@ -0,0 +1,57 @@
+<template functional>
+  <div>
+	<label :class="$style.label" v-if="props.label">
+		<span>{{props.label}}</span>
+		<component
+			:is="$options.components.ElInput"
+			:value="props.value"
+			:type="props.type"
+			:placeholder="props.placeholder"
+			@input="listeners.input"
+		/>
+	</label>
+	<component
+		v-else
+		:is="$options.components.ElInput"
+		:value="props.value"
+		:type="props.type"
+		:placeholder="props.placeholder"
+		@input="listeners.input"
+	/>
+  </div>
+</template>
+
+<script lang="ts">
+import ElInput from "element-ui/packages/input";
+
+import N8nTooltip from "../N8nTooltip";
+
+export default {
+  name: "n8n-input",
+  props: {
+    value: {
+	type: String,
+    },
+    type: {
+	type: String,
+    },
+    placeholder: {
+	type: String,
+    },
+    label: {
+	type: String,
+    }
+  },
+  components: {
+	ElInput,
+	N8nTooltip,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.label {
+	font-weight: var(--font-weight-bold);
+	font-size: var(--font-size-s);
+}
+</style>
---
+++ src/components/N8nTooltip/Tooltip.vue
@@ -0,0 +1,27 @@
+<template functional>
+  <component
+    :is="$options.components.ElTooltip"
+    effect="dark"
+  >
+  </component>
+</template>
+	
+
+<script lang="ts">
+import ElTooltip from "element-ui/packages/tooltip";
+
+export default {
+  name: "n8n-input",
+  props: {
+	 content: {
+
+	 }
+  },
+  components: {
+	ElTooltip,
+  },
+};
+</script>
+
+<style lang="scss" module>
+</style>
